c0d9fa6101578a224f82958018c5264b358acd4e
AtlasBright <brightjackscp@gmail.com>
2024-10-03 23:16:37 +0300

    al'penise

---
+++ hw02_01/hw02_01/hw02_01.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+enum mounth {
+	ending,
+	January,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December,
+
+};
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int mounth_in;
+	std::cout << "Введите номер месяца: ";
+	std::cin >> mounth_in;
+	mounth_in = static_cast<mounth>(mounth_in);
+	switch (mounth_in) {
+		case 0:
+			std::cout << mounth::ending << std::endl;
+			break;
+		case 1:
+			std::cout << mounth::January << std::endl;
+			break;
+	}
+		
+	
+	
+
+
+	return 0;
+}
+
---
+++ hw3/hw3/hw3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+struct bankAcc {
+	int accNum;
+	float balance;
+	std::string accName;
+
+};
+
+void accedit(bankAcc examp, float i) {
+	examp.balance = i;
+	std::cout << "Ваш счёт : " << examp.accNum << std::endl;
+	std::cout << "Имя владельца: " << examp.accName << std::endl;
+	std::cout << "Баланс: " << examp.balance;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int i, num;
+	bankAcc Andre;
+	Andre.accNum = 123456789;
+
+	for (;;) {
+		std::cout << "Введите номер счёта: \n";
+		std::cin >> num;
+		if (Andre.accNum == num) {
+			std::cout << "Введите имя владельца: \n";
+			std::cin >> Andre.accName;
+			std::cout << "Введите баланс: \n";
+			std::cin >> Andre.balance;
+			std::cout << "Введите новый баланс: \n";
+			std::cin >> i;
+			accedit(Andre, i);
+			break;
+		}
+		else
+			std::cout << "не известный номер \n";
+	}
+	return 0;
+}
